c40ff6e533adfc190abd51a9b4c9e6323bf215fb
Jonathan Hammond <hamman359@gmail.com>
2014-10-27 14:12:21 -0400

    Adding next exercise.

---
+++ csharp/anagram/Anagram.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+public class Anagram
+{
+	public object Match(String[] words)
+	{
+		throw new NotImplementedException();
+	}
+
+	public Anagram(string word)
+	{
+
+	}
+}
